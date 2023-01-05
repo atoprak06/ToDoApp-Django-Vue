1e419dcba3f004062e40eaad9a24b18d3c9e83d1
Abdullah Toprak <abdullahtoprakcontact@gmail.com>
2023-01-05 16:03:59 +0300

    todo app views added

--- todoappbackend/todo/views.py
+++ todoappbackend/todo/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from .serializer import TodoSerializer
+from .models import Todo
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+@api_view(['GET', 'POST'])
+def TodoView(request):
+    if request.method == 'GET':
+        queryset = Todo.objects.all()        
+        serializer = TodoSerializer(queryset,many=True)       
+        return Response(serializer.data)
+    
+    if request.method == 'POST':
+        Todo.objects.create(**request.data)
+        return redirect('todo')
+        
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def TodoDetailView(request,id):
+    if request.method == 'GET':
+        queryset = Todo.objects.get(id=id)        
+        serializer = TodoSerializer(queryset,many=False)        
+        return Response(serializer.data)
+    
+    if request.method == 'PUT':      
+        queryset = Todo.objects.filter(id=id).update(**request.data)        
+        return Response(request.data)
+    
+    if request.method == 'DELETE':      
+        queryset = Todo.objects.filter(id=id).delete()     
+        return Response('Object is deleted')
+
+   
