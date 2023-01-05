96bf47fb51248fd26ee58a152b55845be9fafa7a
Abdullah Toprak <abdullahtoprakcontact@gmail.com>
2023-01-05 16:32:11 +0300

    Tasks component created

---
+++ todoappfrontend/src/components/Tasks.vue
@@ -0,0 +1,16 @@
+<template>
+    <div v-for="task in tasks" :key="task.id">
+        <Task @deleteTask="$emit('deleteTask',task.id)" @reminderToggle="$emit('reminderToggle',task.id)" :task = 'task'/>
+    </div>        
+</template>
+
+<script>
+import Task from './Task'
+export default {
+    name:'Tasks',
+    props:{tasks:Array},
+    components:{Task},
+    emits:['reminderToggle','deleteTask']
+
+}
+</script>
