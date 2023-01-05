c94a112d5007dbcc4b9f85c872337291a3d6a527
Abdullah Toprak <abdullahtoprakcontact@gmail.com>
2023-01-05 16:26:09 +0300

    homeview created

--- todoappfrontend/src/views/HomeView.vue
+++ todoappfrontend/src/views/HomeView.vue
@@ -1,18 +1,56 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <AddTask v-show="showAddForm" @addNewTask="addNewTask"/>
+  <Tasks @deleteTask="deleteTask" @reminderToggle="reminderToggle" :tasks='tasks'/>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import AddTask from '../components/AddTask'
+import Tasks from '../components/Tasks'
+import axios from 'axios'
 
 export default {
+  
   name: 'HomeView',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      tasks : [],      
+    }
+  },
+  async created(){
+    await this.getTasks()
+    this.tasks.sort((a, b) => b.id-a.id)
+  },
+  components: {AddTask,Tasks},
+  props:{showAddForm:Boolean},
+  methods:{
+    async reminderToggle(id){        
+      this.tasks.forEach(task=>{
+        if (task.id === id){                  
+          task.reminder = !task.reminder
+          axios.put(`api/v1/todo/${id}`,task)                    
+        }
+      })
+    },
+    async deleteTask(id){
+      this.tasks.forEach(task=>{
+        if (task.id === id){
+          axios.delete(`api/v1/todo/${id}`)
+          this.tasks.splice(this.tasks.indexOf(task),1)
+        }
+      })
+    },
+    async addNewTask(task){      
+      axios.post('api/v1/todo/',task)
+      this.tasks.unshift(task)
+    },
+    async getTasks(){
+        await axios
+          .get('api/v1/todo')
+          .then(response=>{
+            this.tasks = response.data                               
+          }) 
+    }    
   }
 }
 </script>
