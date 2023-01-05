847ac1e3c88e7713f648ed0056befb72e43f69f2
Abdullah Toprak <abdullahtoprakcontact@gmail.com>
2023-01-05 16:32:47 +0300

    button component created

---
+++ todoappfrontend/src/components/Button.vue
@@ -0,0 +1,10 @@
+<template>
+    <button :class="[showAddForm ? 'btn-danger':'btn-primary','btn']">{{showAddForm?'Close':'Add'}}</button>
+</template>
+
+<script>
+export default {
+    name:'Button',
+    props:{showAddForm:Boolean}
+}
+</script>
