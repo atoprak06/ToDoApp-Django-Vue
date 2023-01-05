eadeaab5cf489372d1edc955c94f86cf6277179f
Abdullah Toprak <abdullahtoprakcontact@gmail.com>
2023-01-05 16:31:07 +0300

    Footer component created

---
+++ todoappfrontend/src/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="d-flex flex-column align-items-center justify-content-center">
+        <h3 class="text-dark ">Task Reminder 2023 </h3>
+        <router-link to="about" class=" text-decoration-none text-uppercase fs-6 fw-semibold">about</router-link>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    name:'Footer'
+}
+</script>
