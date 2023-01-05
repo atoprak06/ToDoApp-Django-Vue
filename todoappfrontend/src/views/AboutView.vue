34b82c8707a7b87cce834078c8372c7fa12e5454
Abdullah Toprak <abdullahtoprakcontact@gmail.com>
2023-01-05 16:26:50 +0300

    About view added

--- todoappfrontend/src/views/AboutView.vue
+++ todoappfrontend/src/views/AboutView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h3 class="text-success">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt repellendus nemo sed dolore delectus pariatur adipisci quae? Voluptates, laboriosam quas?</h3>
+    <router-link to="/" class=" text-decoration-none text-primary fs-6">Go back</router-link>
   </div>
 </template>
