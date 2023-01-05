19c1e9dd4e4f732c568ccbd9f803ba22ce92bc63
Abdullah Toprak <abdullahtoprakcontact@gmail.com>
2023-01-05 16:33:18 +0300

    Header component created

---
+++ todoappfrontend/src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="d-flex justify-content-between align-items-center mt-3 pb-2 border-bottom border-2 border-secodary ">
+        <h3 class=" ms-5 text-dark">Task Reminder</h3>
+        <Button :showAddForm="showAddForm" @click.prevent="$emit('toggleAddTask')"/>
+    </div>
+</template>
+
+<script>
+    import Button from './Button'
+   
+
+export default {
+    name:'Header',
+    data(){
+        return{
+            
+        }
+    },
+    emits:['toggleAddTask'],
+    components:{Button},
+    props:{showAddForm:Boolean}
+}
+</script>
